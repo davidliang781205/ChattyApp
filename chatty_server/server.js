161d032c41a54ff81f171b4a4905a2801f561b5e
davidliang781205 <davidliang781205@gmail.com>
2017-04-05 17:06:46 -0700

    Completed broadcast messages

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const SocketServer = WebSocket.Server;
+const uuidV1 = require('uuid/v1');
 
 // Set the port to 1235
 const PORT = 3001;
@@ -12,13 +14,28 @@ const server = express()
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
 
 // Create the WebSockets server
-const wss = new SocketServer({ server });
-
+const wss = new WebSocket.Server({ server });
+wss.broadcast = function broadcast(data) {
+  wss
+    .clients
+    .forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+};
 // Set up a callback that will run when a client connects to the server When a
 // client connects they are assigned a socket, represented by the ws parameter
 // in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  ws.on('message', (message) => {
+    message = JSON.parse(message);
+    message.uuid = uuidV1();
+    message = JSON.stringify(message);
+    console.log(message);
+    wss.broadcast(message);
+  });
 
   // Set up a callback for when a client closes the socket. This usually means
   // they closed their browser.
